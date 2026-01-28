1961ff7d50e8981fd8a309383c611a0b77685364
Mutairu <thebillionnairadream@gmail.com>
2026-01-28 18:23:53 +0100

    added error handler and also added rate limit per req

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const bookRouter = require('./routes/books');
 const logger = require('./middlewares/logger');
 const notFoundPage = require('./middlewares/notFoundPage');
+const errorHandler = require('./middlewares/errorHandler');
+const { rateLimit } = require('express-rate-limit');
 
 const app = express();
+const limiter = rateLimit({
+  limit: 100, // limit per IP req
+  windowMs: 10 * 60 * 1000, //total mins
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
+app.use(limiter);
 
 app.get('/', (req, res) => {
   res.json({ success: true });
@@ -17,5 +24,6 @@ app.use('/api/books', bookRouter);
 
 // error
 app.use(notFoundPage);
+app.use(errorHandler);
 
 module.exports = app;
